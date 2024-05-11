0c083edbb22d4ab3c4de2dba84df268b6922f8c5
Sergi-0 <144801688+Sergi-0@users.noreply.github.com>
2024-05-12 01:04:05 +0300

    Add files via upload

---
+++ lab8.1.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+using namespace std;
+
+template<class T>
+class MyShared {
+    T* p = nullptr;
+    
+
+public:
+    int* n = nullptr;// Счетчик ссылок
+
+    MyShared(T* p = nullptr) : p(p), n(new int(1)) {}// Конструктор
+
+    
+    ~MyShared() {// Деструктор
+        if ((*n)-1 == 0) {
+            delete p;
+            delete n;
+        }
+    }
+
+    
+    MyShared(const MyShared& t) : p(t.p), n(t.n) {// Конструктор копирования
+        (*n)++;
+    }
+
+    
+    MyShared& operator=(const MyShared& t) {// Оператор присваивания копированием
+        if (this != &t) {
+            if ((*n)-1 == 0) {
+                delete p;
+                delete n;
+            }
+            p = t.p;
+            n = t.n;
+            (*n)++;
+        }
+        return *this;
+    }
+
+    
+    MyShared(MyShared&& t) : p(t.p), n(t.n) {// Конструктор перемещения
+        t.p = nullptr;
+        t.n = new int(0); 
+    }
+
+    
+    MyShared& operator=(MyShared&& t) {// Оператор присваивания перемещением
+        if (this != &t) {
+            if ((*n)-1 == 0) {
+                delete p;
+                delete n;
+            }
+            p = t.p;
+            n = t.n;
+            t.p = nullptr;
+            t.n = new int(0); 
+        }
+        return *this;
+    }
+
+    
+    T* get() const { return p; }
+
+    
+    T& operator*() { return *p; }
+
+   
+    T* operator->() { return p; } 
+};
+
+template<typename T, typename... Args>
+MyShared<T> Make_MyShared(Args&&... args) {
+    return MyShared<T>(new T(forward<Args>(args)...));
+}
+
+class MyPoint {
+public:
+    int x, y;
+    MyPoint(int x, int y) : x(x), y(y) {}
+    void vivod() const { 
+        cout << "Point(" << x << ", " << y << ")" << endl; 
+    }
+};
+
+int main() {
+    auto PtSr = Make_MyShared<MyPoint>(10, 20);
+    PtSr->vivod();
+    MyShared<MyPoint> PtSr2 = PtSr;
+    PtSr2->vivod(); 
+    cout << *PtSr2.n << endl;
+    cout << *PtSr.n;
+}
---
+++ lab8.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <utility>
+using namespace std;
+
+template<class T>
+class MyUnique
+{
+    T* p = nullptr;
+public:
+    MyUnique(T* p) : p(p) {}; // конструктор
+
+    MyUnique(MyUnique&& t) { // конструктор перемещения
+        p = t.p; 
+        t.p = nullptr;
+    } 
+
+    MyUnique(const MyUnique&) = delete; // конструктор копирования (запрещен)
+
+    ~MyUnique() { // деструктор
+        delete p;
+    }
+
+    T* get() const {
+        return p;
+    }
+
+    T& operator*() { // перегрузка *
+        return *p;
+    }
+
+    T* operator->() { // перегрузка ->, хоть код такой же как и в get(), работает это подругому
+        return p;
+    }
+
+    MyUnique& operator=(const MyUnique&) = delete; // оператор присваивания копированием (запрещен)
+
+    MyUnique& operator=(MyUnique&& t) { // оператор перемещения
+        p = t.p;
+        t.p = nullptr;
+        return *this;
+    }
+
+};
+
+template<typename T, typename... Args>
+MyUnique<T> Make_MyUnique(Args&&... args) {
+    return MyUnique<T>(new T(forward<Args>(args)...));
+}
+
+class MyPoint { // класс точка для примера
+public:
+    int x, y;
+
+    MyPoint(int x, int y) : x(x), y(y) {}
+
+    void vivod() const { 
+        cout << "Point(" << x << ", " << y << ")" << endl; 
+    }
+};
+
+int main() {
+    auto PtUq = Make_MyUnique<MyPoint>(10, 20);
+    PtUq->vivod(); 
+    MyUnique<MyPoint> mvPt = move(PtUq);
+    mvPt->vivod(); 
+}
